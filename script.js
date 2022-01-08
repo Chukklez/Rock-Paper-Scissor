23a57c11a2e20b17fbe96ec5b657e8ff4f83af79
Bastian <bastian.bjorn@gmail.com>
2022-01-08 18:55:49 +0100

    finished playRound() and connected them to get a functioning round

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
 // create a function that randomly return either "Rock", "Paper", or "Scissor" and output it into the console.log
 
+
 // Create a function that randomizes a computer play
 function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
-// assign the randomized value to a variable
-let computerResult = computerPlay();
 
-// choose which number should be assigned to rock paper and scissor
-switch (computerResult) {
-  case 0:
-    console.log('rock');
-    break;
-  case 1:
-    console.log('paper');
-    break;
-  case 2:
-    console.log('scissor');
-    break;
-}
+computerPlay = computerPlay();
+
 // output it to the console log
+// console.log(computerPlay);
 
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
+
 // Create a prompt that asks for the userinput
 // let userInput = prompt("Enter Rock, Paper or Scissor");
+
+
+// Make the userinput case-insensitive
+// the if statement can have lowercase
+// use toLowerCase() Function
+// the input has to be converted to lowercase
+// So no matter what the player inputs it will be converted to lowercase and THEN compared to the if statement.
+
+
+
 // Write a function that has two parameters, playerSelection and computerSelection
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock' && computerSelection === 'scissor') {
-    return 'Rock Beats Scissors! You Won!';
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'Paper beats Rock! Computer won!';
-  } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-    return "It is a tie! Nobody won!";
-  } else if (playerSelection === "scissor" && computerSelection === "rock") {
-      return "Rock beats Scissor! Computer Wins!"
+  if (playerSelection == 'Rock' && computerSelection == 2) {
+    return 'You play: ' + playerSelection + "!" + ' Rock Beats Scissors! You Win!';
+  } else if (playerSelection == 'Rock' && computerSelection == 1) {
+    return 'You play: ' + playerSelection + "!" + ' Paper beats Rock! You lose!';
+  } else if (playerSelection == 'Rock' && computerSelection == 0) {
+    return 'You play: ' + playerSelection + "!" + ' It is a tie! Nobody won!';
+  } else if (playerSelection == 'Scissor' && computerSelection == 1) {
+    return 'You play: ' + playerSelection + "!"  + ' Scissor beats Paper! You win!';
+  } else if (playerSelection == 'Scissor' && computerSelection == 0) {
+    return 'You play: ' + playerSelection + "!"  + ' Rock beats Scissor! You lose!';
+  } else if (playerSelection == 'Scissor' && computerSelection == 2) {
+    return 'You play: ' + playerSelection + "!"  + ' It is a tie! Nobody won!';
+  } else if (playerSelection == 'Paper' && computerSelection == 0) {
+    return 'You play: ' + playerSelection + "!"  + ' Paper beats Rock! You win!';
+  } else if (playerSelection == 'Paper' && computerSelection == 2) {
+    return 'You play: ' + playerSelection + "!"  + ' Scissor beats Paper! You lose!';
+  } else if (playerSelection == 'Paper' && computerSelection == 1) {
+    return 'You play: ' + playerSelection + "!"  + ' It is a tie! Nobody won!';
   }
 }
-console.log(playRound());
-// Make the userinput case-insensitive
-// Make a variable that stores the userinput
+
+const playerSelection = "Paper";
+const computerSelection = computerPlay;
+
+// choose which number should be assigned to the computers rock paper and scissor
+switch (computerPlay) {
+  case 0:
+    console.log('Computer played Rock');
+    break;
+  case 1:
+    console.log('Computer played Paper');
+    break;
+  case 2:
+    console.log('Computer played Scissor');
+    break;
+}
+
+// output the round to the console
+console.log(playRound(playerSelection, computerSelection));
