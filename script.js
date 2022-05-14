56a24eb5b081bba594d585ff02f0d053b83d0331
Bastian <bastian.bjorn@gmail.com>
2022-05-14 21:03:32 +0200

    changed the switch statement and added strings to them so that insteado of 0, 1, 2 it says rock, paper or scissors. Changed the template literals to display the variables instead of hardcoded results

--- script.js
+++ script.js
@@ -3,54 +3,58 @@ function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
 
+
 computerPlay = computerPlay(); //assigning the function computerPlay() to the variable computerPlay 
 
-// choose which number should be assigned to the computers rock paper and scissor
-switch (computerPlay) {
-  case 0: //Rock
-    break;
-  case 1: // Paper
-    break;
-  case 2: // Scissors
-    break;
-}
+// choose which number should be assigned to the computers rock paper and scissors
 
 const playerAnswer = "paper" //The player input
 const playerSelection = playerAnswer.toLowerCase(); //Take the playerinput and convert it to lowercase in a new variable called playerSelection
-const computerSelection = computerPlay; //assign the computerPlay variable into the computerSelection variable
+let computerSelection = computerPlay; //assign the computerPlay variable into the computerSelection variable
 let result = ""
+switch (computerPlay) {
+  case 0: //Rock
+  computerSelection = "rock";
+    break;
+  case 1: // Paper
+  computerSelection = "paper";
+    break;
+  case 2: // Scissors
+  computerSelection = "scissors";
+    break;
+}
 
 // Create separate if statements for each selection with 1 if, 1 else if and then an else.
 function playRound(playerSelection, computerSelection) {
   // If player chooses Rock
-  if (playerSelection == 'rock' && computerSelection == 2) {
-    result = `You play ${playerSelection} and computer played scissors!
-    ${playerSelection} beats scissors! You win!`
-  } else if (playerSelection == 'rock' && computerSelection == 1) {
-    result = `You play ${playerSelection} and computer played paper! 
-    paper beats rock! You lose!`;
-  } else if (playerSelection == 'rock' && computerSelection == 0) {
-    result = `You play ${playerSelection} and computer played rock!
+  if (playerSelection == 'rock' && computerSelection == "scissors") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}!
+    ${playerSelection} beats ${computerSelection}! You win!`
+  } else if (playerSelection == 'rock' && computerSelection == "paper") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}! 
+    ${computerSelection} beats ${playerSelection}! You lose!`;
+  } else if (playerSelection == 'rock' && computerSelection == "rock") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}!
     It is a tie! Nobody won!`;
     //If player chooses Scissors
-  } else if (playerSelection == 'scissors' && computerSelection == 1) {
-    result = `You play ${playerSelection} and computer played paper!  
-    ${playerSelection} beats paper! You win!`
-  } else if (playerSelection == 'scissors' && computerSelection == 0) {
+  } else if (playerSelection == 'scissors' && computerSelection == "paper") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}!  
+    ${playerSelection} beats ${computerSelection}! You win!`
+  } else if (playerSelection == 'scissors' && computerSelection == "rock") {
     result = `You play ${playerSelection} and computer played rock! 
-    rock beats scissors! You lose!`
-  } else if (playerSelection == 'scissors' && computerSelection == 2) {
+    rock beats ${playerSelection}! You lose!`
+  } else if (playerSelection == 'scissors' && computerSelection == "scissors") {
     result = `You play ${playerSelection} and computer played scissors! 
     It is a tie! Nobody won!`;
     //If player chooses paper
-  } else if (playerSelection == 'paper' && computerSelection == 0) {
-    result = `You play ${playerSelection} and computer played rock!
-    ${playerSelection} beats rock! You win!`
-  } else if (playerSelection == 'paper' && computerSelection == 2) {
-    result = `You play ${playerSelection} and computer played scissors! 
-    scissors beats paper! You lose!`
-  } else if (playerSelection == 'paper' && computerSelection == 1) {
-    result = `You play ${playerSelection} and computer played paper! 
+  } else if (playerSelection == 'paper' && computerSelection == "rock") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}!
+    ${playerSelection} beats ${computerSelection}! You win!`
+  } else if (playerSelection == 'paper' && computerSelection == "scissors") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}! 
+    ${computerSelection} beats ${playerSelection}! You lose!`
+  } else if (playerSelection == 'paper' && computerSelection == "paper") {
+    result = `You play ${playerSelection} and computer played ${computerSelection}! 
     It is a tie! Nobody won!`;
   } return result;
 } 
