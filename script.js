c52e21c1861901ea7a2ec01f3e2786c36dd080bc
Bastian <bastian.bjorn@gmail.com>
2022-05-14 20:30:29 +0200

    moved the code around so that they're in a more cascading order, removed some console logs and comments. Cleaning up the code a bit for easier readability and added an empty result variable to return the winner

--- script.js
+++ script.js
@@ -1,85 +1,68 @@
-// create a function that randomly return either "Rock", "Paper", or "Scissor" and output it into the console.log
-
-
 // Create a function that randomizes a computer play
 function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
 
-computerPlay = computerPlay();
-
-// output it to the console log
-// console.log(computerPlay);
-
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-
-// Create a prompt that asks for the userinput
-// let userInput = prompt("Enter Rock, Paper or Scissor");
-
-
-// Make the userinput case-insensitive
-// the if statement can have lowercase
-// use toLowerCase() Function
-// the input has to be converted to lowercase
-// So no matter what the player inputs it will be converted to lowercase and THEN compared to the if statement.
-
-
-
-// Write a function that has two parameters, playerSelection and computerSelection
-// Create separate if statements for each selection with 1 if, 1 if else and then an else.
-function playRound(playerSelection, computerSelection) {
-  // If player chooses Rock
-  if (playerSelection == 'rock' && computerSelection == 2) {
-    return `You play ${playerSelection}! 
-    ${playerSelection} beats scissors! You win!`
-  } else if (playerSelection == 'rock' && computerSelection == 1) {
-    return `You play ${playerSelection}! 
-    paper beats rock! You lose!`;
-  } else if (playerSelection == 'rock' && computerSelection == 0) {
-    return `You play ${playerSelection}! 
-    It is a tie! Nobody won!`;
-    //If player chooses Scissors
-  } else if (playerSelection == 'scissors' && computerSelection == 1) {
-    return `You play ${playerSelection}! 
-    ${playerSelection} beats paper! You win!`
-  } else if (playerSelection == 'scissors' && computerSelection == 0) {
-    return `You play ${playerSelection}! 
-    rock beats scissor! You lose!`
-  } else if (playerSelection == 'scissors' && computerSelection == 2) {
-    return `You play ${playerSelection}! 
-    It is a tie! Nobody won!`;
-    //If player chooses paper
-  } else if (playerSelection == 'paper' && computerSelection == 0) {
-    return `You play ${playerSelection}! 
-    ${playerSelection} beats rock! You win!`
-  } else if (playerSelection == 'paper' && computerSelection == 2) {
-    return `You play ${playerSelection}! 
-    scissors beats paper! You lose!`
-  } else if (playerSelection == 'paper' && computerSelection == 1) {
-    return `You play ${playerSelection}! 
-    It is a tie! Nobody won!`;
-  }
-}
-
-// add a second const that uses toLowerCase()
-const playerAnswer = "ScissoRs"
-const playerSelection = playerAnswer.toLowerCase();
-const computerSelection = computerPlay;
+computerPlay = computerPlay(); //assigning the function computerPlay() to the variable computerPlay 
 
 // choose which number should be assigned to the computers rock paper and scissor
 switch (computerPlay) {
-  case 0:
-    console.log('Computer played rock');
+  case 0: //Rock
     break;
-  case 1:
-    console.log('Computer played paper');
+  case 1: // Paper
     break;
-  case 2:
-    console.log('Computer played scissors');
+  case 2: // Scissors
     break;
 }
 
+const playerAnswer = "paper" //The player input
+const playerSelection = playerAnswer.toLowerCase(); //Take the playerinput and convert it to lowercase in a new variable called playerSelection
+const computerSelection = computerPlay; //assign the computerPlay variable into the computerSelection variable
+let result = ""
+
+// Create separate if statements for each selection with 1 if, 1 else if and then an else.
+function playRound(playerSelection, computerSelection) {
+  // If player chooses Rock
+  if (playerSelection == 'rock' && computerSelection == 2) {
+    result = `You play ${playerSelection} and computer played scissors!
+    ${playerSelection} beats scissors! You win!`
+  } else if (playerSelection == 'rock' && computerSelection == 1) {
+    result = `You play ${playerSelection} and computer played paper! 
+    paper beats rock! You lose!`;
+  } else if (playerSelection == 'rock' && computerSelection == 0) {
+    result = `You play ${playerSelection} and computer played rock!
+    It is a tie! Nobody won!`;
+    //If player chooses Scissors
+  } else if (playerSelection == 'scissors' && computerSelection == 1) {
+    result = `You play ${playerSelection} and computer played paper!  
+    ${playerSelection} beats paper! You win!`
+  } else if (playerSelection == 'scissors' && computerSelection == 0) {
+    result = `You play ${playerSelection} and computer played rock! 
+    rock beats scissors! You lose!`
+  } else if (playerSelection == 'scissors' && computerSelection == 2) {
+    result = `You play ${playerSelection} and computer played scissors! 
+    It is a tie! Nobody won!`;
+    //If player chooses paper
+  } else if (playerSelection == 'paper' && computerSelection == 0) {
+    result = `You play ${playerSelection} and computer played rock!
+    ${playerSelection} beats rock! You win!`
+  } else if (playerSelection == 'paper' && computerSelection == 2) {
+    result = `You play ${playerSelection} and computer played scissors! 
+    scissors beats paper! You lose!`
+  } else if (playerSelection == 'paper' && computerSelection == 1) {
+    result = `You play ${playerSelection} and computer played paper! 
+    It is a tie! Nobody won!`;
+  } return result;
+} 
+
+
+
+//Create a new function called game() that calls the playRound function to play a 5 round game that keeps score and reports a winner or lose at the end.
+function game() {
+  return playRound()
+}
+
+
 // output the round to the console
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+game();
