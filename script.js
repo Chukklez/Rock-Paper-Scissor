fd94fc00fc99230b1515cb1679c4bbafe357c42f
Bastian <bastian.bjorn@gmail.com>
2022-05-14 21:16:46 +0200

    cleaned up the code a little bit and removed/changed some comments

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ function computerPlay() {
   return Math.floor(Math.random() * 3);
 }
 
-
 computerPlay = computerPlay(); //assigning the function computerPlay() to the variable computerPlay 
 
-// choose which number should be assigned to the computers rock paper and scissors
 
 const playerAnswer = "paper" //The player input
 const playerSelection = playerAnswer.toLowerCase(); //Take the playerinput and convert it to lowercase in a new variable called playerSelection
-let computerSelection = computerPlay; //assign the computerPlay variable into the computerSelection variable
+let computerSelection = computerPlay; //Assign the computerPlay variable into the computerSelection variable
 let result = ""
 switch (computerPlay) {
   case 0: //Rock
@@ -24,7 +22,7 @@ switch (computerPlay) {
     break;
 }
 
-// Create separate if statements for each selection with 1 if, 1 else if and then an else.
+// Create the playRound function that runs one single round of the game.
 function playRound(playerSelection, computerSelection) {
   // If player chooses Rock
   if (playerSelection == 'rock' && computerSelection == "scissors") {
@@ -59,13 +57,15 @@ function playRound(playerSelection, computerSelection) {
   } return result;
 } 
 
-
-
 //Create a new function called game() that calls the playRound function to play a 5 round game that keeps score and reports a winner or lose at the end.
 function game() {
   return playRound(playerSelection, computerSelection)
 }
 
+//create a functions that keeps track of rounds called getRounds()
+function getRounds(){
+  for (let i = 0; i < 5; i++);
+}
 
 // output the round to the console
 // console.log(playRound(playerSelection, computerSelection));
