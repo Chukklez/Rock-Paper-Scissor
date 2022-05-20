9d8cf40eee9de212811f889f35f7ceb7cca89fe1
Bastian <bastian.bjorn@gmail.com>
2022-05-20 14:22:29 +0200

    commented out non-valid code

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ computerPlay = computerPlay(); //assigning the function computerPlay() to the va
 const playerAnswer = "rock" //The player input
 const playerSelection = playerAnswer.toLowerCase(); //Take the playerinput and convert it to lowercase in a new variable called playerSelection
 let computerSelection = computerPlay; //Assign the computerPlay variable into the computerSelection variable
-let result = ""
+// let result = ""
 switch (computerPlay) {
   case 0: //Rock
   computerSelection = "rock";
@@ -26,46 +26,53 @@ switch (computerPlay) {
 function playRound(playerSelection, computerSelection) {
   // If player chooses Rock
   if (playerSelection == 'rock' && computerSelection == "scissors") {
-    result = `You play ${playerSelection} and computer played ${computerSelection}!
+    `You play ${playerSelection} and computer played ${computerSelection}!
     ${playerSelection} beats ${computerSelection}! You win!`
   } else if (playerSelection == 'rock' && computerSelection == "paper") {
-    result = `You play ${playerSelection} and computer played ${computerSelection}! 
+    `You play ${playerSelection} and computer played ${computerSelection}! 
     ${computerSelection} beats ${playerSelection}! You lose!`;
   } else if (playerSelection ==  computerSelection) {
-    result = `You play ${playerSelection} and computer played ${computerSelection}!
+    `You play ${playerSelection} and computer played ${computerSelection}!
     It is a tie! Nobody won!`;
     //If player chooses Scissors
   } else if (playerSelection == 'scissors' && computerSelection == "paper") {
-    result = `You play ${playerSelection} and computer played ${computerSelection}!  
+    `You play ${playerSelection} and computer played ${computerSelection}!  
     ${playerSelection} beats ${computerSelection}! You win!`
   } else if (playerSelection == 'scissors' && computerSelection == "rock") {
-    result = `You play ${playerSelection} and computer played rock! 
+    `You play ${playerSelection} and computer played rock! 
     rock beats ${playerSelection}! You lose!`
   } else if (playerSelection ==  computerSelection) {
-    result = `You play ${playerSelection} and computer played scissors! 
+    `You play ${playerSelection} and computer played scissors! 
     It is a tie! Nobody won!`;
     //If player chooses paper
   } else if (playerSelection == 'paper' && computerSelection == "rock") {
-    result = `You play ${playerSelection} and computer played ${computerSelection}!
+    `You play ${playerSelection} and computer played ${computerSelection}!
     ${playerSelection} beats ${computerSelection}! You win!`
   } else if (playerSelection == 'paper' && computerSelection == "scissors") {
-    result = `You play ${playerSelection} and computer played ${computerSelection}! 
+    `You play ${playerSelection} and computer played ${computerSelection}! 
     ${computerSelection} beats ${playerSelection}! You lose!`
   } else if (playerSelection ==  computerSelection) {
-    result = `You play ${playerSelection} and computer played ${computerSelection}! 
+    `You play ${playerSelection} and computer played ${computerSelection}! 
     It is a tie! Nobody won!`;
-  } return result;
+  }
 } 
 
+//create a functions that keeps track of rounds called getRounds()
+// function getRounds(){
+//   i = 5;
+//   for (let i = 0; i < 5; i++);
+//   return `Round ${i} out of 5`
+// }
 //Create a new function called game() that calls the playRound function to play a 5 round game that keeps score and reports a winner or lose at the end.
 function game() {
-  return playRound(playerSelection, computerSelection)
+  playRound(playerSelection, computerSelection)
 }
 
-//create a functions that keeps track of rounds called getRounds()
-function getRounds(){
-  for (let i = 0; i < 5; i++);
-}
+
+// count points for either player or computer 
+// playerScore = 0;
+// computerScore = 0;
+
 
 // output the round to the console
 // console.log(playRound(playerSelection, computerSelection));
