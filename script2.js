ab82a5492561897250e2a2dad9fb915a4ee349c1
Bastian <bastian.bjorn@gmail.com>
2022-12-11 20:59:42 +0100

    added use strict to keep it consistent

--- script2.js
+++ script2.js
@@ -1,6 +1,9 @@
+"use strict";
+
 const rock = 0;
 const paper = 1;
 const scissor = 2;
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
@@ -8,4 +11,4 @@ function getComputerChoice() {
 
 const computerChoice = `Computer chooses ${getComputerChoice()}`;
 
-console.log(computerChoice);
+console.log(rock);
